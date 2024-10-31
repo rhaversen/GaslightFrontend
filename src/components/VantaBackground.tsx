04604616bfa424cbb3e4e9e7133ba35b3b9f91c5
Rasmus Haversen <rhaversen@gmail.com>
2024-10-31 20:27:26 +0100

    Add vanta effects

---
+++ src/components/VantaBackground.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import HALO from 'vanta/dist/vanta.halo.min.js'
+import NET from 'vanta/dist/vanta.net.min.js'
+
+export const Halo = (): JSX.Element => {
+	const vantaRef = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		let vantaEffect: any = null
+		if (vantaRef.current !== null) {
+			vantaEffect = HALO({
+				el: vantaRef.current,
+				mouseControls: false,
+				touchControls: false,
+				gyroControls: false,
+				minHeight: 200.0,
+				minWidth: 200.0,
+				baseColor: 0xf9202c,
+				backgroundColor: 0x60610,
+				amplitudeFactor: 1.0,
+				size: 2,
+				THREE
+			})
+		}
+
+		return () => {
+			if (vantaEffect !== null) vantaEffect.destroy()
+		}
+	}, [])
+
+	return <div ref={vantaRef} className="absolute w-full h-full -z-10" />
+}
+
+export const Net = (): JSX.Element => {
+	const vantaRef = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		let vantaEffect: any = null
+		if (vantaRef.current !== null) {
+			vantaEffect = NET({
+				el: vantaRef.current,
+				mouseControls: true,
+				touchControls: true,
+				gyroControls: false,
+				minHeight: 200.00,
+				minWidth: 200.00,
+				scale: 1.00,
+				scaleMobile: 1.00,
+				color: 0xbd7a43,
+				backgroundColor: 0x29262c,
+				points: 16.00,
+				maxDistance: 18.00,
+				spacing: 19.00,
+				showDots: false,
+				THREE
+			})
+		}
+
+		return () => {
+			if (vantaEffect !== null) vantaEffect.destroy()
+		}
+	}, [])
+
+	return <div ref={vantaRef} className="absolute w-full h-full -z-10" />
+}
---
+++ src/types/vanta.d.ts
@@ -0,0 +1,9 @@
+declare module 'vanta/dist/vanta.halo.min.js' {
+	const HALO: any
+	export default HALO
+}
+
+declare module 'vanta/dist/vanta.net.min.js' {
+	const NET: any
+	export default NET
+}
