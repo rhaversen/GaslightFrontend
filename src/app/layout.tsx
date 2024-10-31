e55941818c1fa129452b624fcadf6a2444a757f1
Rasmus Haversen <rhaversen@gmail.com>
2024-10-31 17:37:54 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import ErrorProvider from '@/contexts/ErrorContext/ErrorProvider'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { type ReactElement } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Gaslight',
+		default: 'Gaslight'
+	},
+	description: 'Gaslight',
+	alternates: {
+		canonical: 'https://www.gaslight.fun'
+	},
+	icons: {
+		icon: '/favicon.ico'
+	}
+}
+
+export default function RootLayout ({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>): ReactElement {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ErrorProvider>
+					{children}
+				</ErrorProvider>
+			</body>
+		</html>
+	)
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import React, { type ReactElement } from 'react'
+
+export default function Page (): ReactElement {
+	return (
+		<></>
+	)
+}
