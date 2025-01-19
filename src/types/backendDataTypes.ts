7440bfc324bbc2181395213f2eb6dc57caecf085
Rasmus Haversen <rhaversen@gmail.com>
2025-01-20 00:06:03 +0100

    Add timestamps and submission details to backend data types

--- src/types/backendDataTypes.ts
+++ src/types/backendDataTypes.ts
@@ -13,4 +13,29 @@ export interface UserType {
 	expirationDate?: Date
 	/** Date when the password reset code will expire */
 	passwordResetExpirationDate?: Date
+
+	// Timestamps
+	createdAt: Date
+	updatedAt: Date
+}
+
+export interface ISubmission extends Document {
+	/** ID of the submission */
+	_id: string
+	/** Title of the submission */
+	title: string
+	/** Code submitted by the user */
+	code: string
+	/** User who submitted the code */
+	user: UserType['_id']
+	/** Decides if the submission is part of the tournament (Can only have one active submission per user) */
+	active: boolean
+	/** Decides if the submission has passed an evaluation and is ready for tournaments */
+	passedEvaluation: boolean
+	/** The lines of code in the submission */
+	loc: number
+
+	// Timestamps
+	createdAt: Date
+	updatedAt: Date
 }
