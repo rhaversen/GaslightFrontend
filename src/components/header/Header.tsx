4349a20360340c30ef5d9e70cba24744a91fcf07
Rasmus Haversen <rhaversen@gmail.com>
2025-01-19 07:22:26 +0100

    refactor header component to have left and right page buttons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,30 +12,44 @@ const Header = (): ReactElement => {
 		setMounted(true)
 	}, [])
 
-	const authenticatedButtons: Record<string, string> = {
-		Profile: '/profile',
+	// Unauthenticated buttons
+	const authenticatedButtonsLeft: Record<string, string> = {
 		'Overall Ranking': '/',
 		'Last Tournament': '/'
 	}
+	const authenticatedButtonsRight: Record<string, string> = {
+		Profile: '/profile'
+	}
 
-	const unauthenticatedButtons: Record<string, string> = {
-		Login: '/login',
-		'Sign Up': '/signup',
+	// Authenticated buttons
+	const unauthenticatedButtonsLeft: Record<string, string> = {
 		'Overall Ranking': '/',
 		'Last Tournament': '/'
+	}
+	const unauthenticatedButtonsRight: Record<string, string> = {
+		'Log in': '/login',
+		'Sign Up': '/signup'
 	}
 
 	if (!mounted) {
 		return <div className='p-5 absolute'></div> // Return empty container while mounting
 	}
 
-	const buttons = currentUser !== null ? authenticatedButtons : unauthenticatedButtons
+	const buttonsLeft = currentUser !== null ? authenticatedButtonsLeft : unauthenticatedButtonsLeft
+	const buttonsRight = currentUser !== null ? authenticatedButtonsRight : unauthenticatedButtonsRight
 
 	return (
-		<div className='p-5 absolute'>
-			{Object.entries(buttons).map(([title, path]) => (
-				<Button key={title} title={title} path={path} />
-			))}
+		<div className='p-5 absolute flex justify-between w-full'>
+			<div className='flex'>
+				{Object.entries(buttonsLeft).map(([title, path]) => (
+					<Button key={title} title={title} path={path} />
+				))}
+			</div>
+			<div className='flex'>
+				{Object.entries(buttonsRight).map(([title, path]) => (
+					<Button key={title} title={title} path={path} />
+				))}
+			</div>
 		</div>
 	)
 }
