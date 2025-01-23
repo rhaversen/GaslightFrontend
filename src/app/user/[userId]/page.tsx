b11d1e782658943b2252ef2a5bc8972cd84520d2
Rasmus Haversen <rhaversen@gmail.com>
2025-01-23 06:31:49 +0100

    Refactor out LoadingPlaceholder to seperate component

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import Link from 'next/link'
 import React, { type ReactElement, useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import LoadingPlaceholder from '@/components/LoadingPlaceholder'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
@@ -41,18 +42,7 @@ export default function Page ({ params }: Readonly<{ params: { userId: string } 
 
 	if (isLoading) {
 		return (
-			<main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
-				<div className="container mx-auto max-w-4xl p-6">
-					<div className="animate-pulse space-y-6">
-						<div className="h-10 bg-gray-200 rounded-lg w-1/3 mx-auto"></div>
-						<div className="space-y-4 bg-gray-50 p-6 rounded-xl">
-							<div className="h-4 bg-gray-200 rounded w-3/4"></div>
-							<div className="h-4 bg-gray-200 rounded w-2/3"></div>
-							<div className="h-4 bg-gray-200 rounded w-1/2"></div>
-						</div>
-					</div>
-				</div>
-			</main>
+			<LoadingPlaceholder />
 		)
 	}
 
---
+++ src/components/LoadingPlaceholder.tsx
@@ -0,0 +1,18 @@
+const LoadingPlaceholder = () => {
+    return (
+        <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+            <div className="container mx-auto max-w-4xl p-6">
+                <div className="animate-pulse space-y-6">
+                    <div className="h-10 bg-gray-200 rounded-lg w-1/3 mx-auto"></div>
+                    <div className="space-y-4 bg-gray-50 p-6 rounded-xl">
+                        <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+                        <div className="h-4 bg-gray-200 rounded w-2/3"></div>
+                        <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default LoadingPlaceholder
