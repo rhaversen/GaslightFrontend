2d7a867d50a2c66cb61ce55ca746596190c85481
Rasmus Haversen <rhaversen@gmail.com>
2025-01-24 07:46:35 +0100

    Handle viewing of other users profile

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -16,13 +16,9 @@ export default function Page ({ params }: Readonly<{ params: { userId: string } 
 	const isOwnProfile = currentUser?._id === params.userId
 	const router = useRouter()
 	const [isLoading, setIsLoading] = useState(true)
+	const [username, setUsername] = useState<string>('')
 
 	useEffect(() => {
-		if (!isOwnProfile) {
-			router.push('/')
-			return
-		}
-
 		setIsLoading(true)
 		const fetchUser = async (): Promise<void> => {
 			try {
@@ -31,6 +27,7 @@ export default function Page ({ params }: Readonly<{ params: { userId: string } 
 					{ withCredentials: true }
 				)
 				setUserData(response.data)
+				setUsername(response.data.username ?? 'Unknown User')
 			} catch (error) {
 				console.error('Error fetching user:', error)
 			} finally {
@@ -50,7 +47,7 @@ export default function Page ({ params }: Readonly<{ params: { userId: string } 
 		<main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
 			<div className="container mx-auto max-w-4xl p-2">
 				<h1 className="w-full sm:w-auto sm:flex-1 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 text-center order-last sm:order-none m-8 pb-2">
-					{'User Profile\r'}
+					{isOwnProfile ? 'Your Profile' : `${username}'s Profile`}
 				</h1>
 				{userData !== null && (
 					<div className="space-y-6">
@@ -60,10 +57,12 @@ export default function Page ({ params }: Readonly<{ params: { userId: string } 
 									<span className="font-medium">{'Username'}</span>
 									<span className="text-gray-900">{userData.username}</span>
 								</p>
-								<p className="flex justify-between items-center text-gray-600">
-									<span className="font-medium">{'Email'}</span>
-									<span className="text-gray-900">{userData.email}</span>
-								</p>
+								{(userData.email !== null) && (
+									<p className="flex justify-between items-center text-gray-600">
+										<span className="font-medium">{'Email'}</span>
+										<span className="text-gray-900">{userData.email}</span>
+									</p>
+								)}
 								<p className="flex justify-between items-center text-gray-600">
 									<span className="font-medium">{'Member since'}</span>
 									<span className="text-gray-900">{new Date(userData.createdAt).toLocaleDateString()}</span>
