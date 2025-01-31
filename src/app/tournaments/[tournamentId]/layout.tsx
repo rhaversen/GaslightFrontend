f666d831367b0a514b1ecf97c91d61683c1078d0
Rasmus Haversen <rhaversen@gmail.com>
2025-01-31 14:06:55 +0100

    Add initial tournament view

---
+++ src/app/tournaments/[tournamentId]/layout.tsx
@@ -0,0 +1,30 @@
+import { type Metadata } from 'next'
+
+export async function generateMetadata({ params }: Readonly<{
+	children: React.ReactNode
+	params: Promise<{ tournamentId: string }>
+}>): Promise<Metadata> {
+	const resolvedParams = await params
+	return {
+		title: {
+			template: '%s | Gaslight',
+			default: 'Viewing Tournament'
+		},
+		alternates: {
+			canonical: `https://www.gaslight.fun/tournaments/${resolvedParams.tournamentId}`
+		}
+	}
+}
+
+export default function TournamentLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+	params: Promise<{ tournamentId: string }>
+}>): React.JSX.Element {
+	return (
+		<>
+			{children}
+		</>
+	)
+}
