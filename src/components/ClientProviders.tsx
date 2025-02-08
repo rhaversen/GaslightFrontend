e032e71856a7a231642bf0ce417029f6cbeaa40a
Rasmus Haversen <rhaversen@gmail.com>
2025-02-08 16:53:40 +0100

    Add @tanstack/react-query for data fetching and wrap client providers in QueryClientProvider

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,14 +1,20 @@
+'use client'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ErrorProvider from '@/contexts/ErrorContext/ErrorProvider'
 import UserProvider from '@/contexts/UserProvider'
 import { type ReactNode } from 'react'
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
-
+	const [queryClient] = useState(() => new QueryClient())
+	
 	return (
-		<ErrorProvider>
-			<UserProvider>
-				{children}
-			</UserProvider>
-		</ErrorProvider>
+		<QueryClientProvider client={queryClient}>
+			<ErrorProvider>
+				<UserProvider>
+					{children}
+				</UserProvider>
+			</ErrorProvider>
+		</QueryClientProvider>
 	)
 }
